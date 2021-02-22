7703105d8a7b9c9aff4d85469a3bbaec594e7460
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-02-22 22:45:44 +0100

    ajout fonctionnalité et syle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
+const btn = document.querySelector('.enter');
+const bgImg = document.querySelector('.bg_img');
+btn.addEventListener('click', () => {
+    bgImg.classList.add('fadeout')
+    // bgImg.style.display = 'none';
+})
+
+const defile = document.querySelectorAll('.defile');
+const toggledElement = document.querySelectorAll('.toggle');
+for(let i = 0; i < defile.length; i++) {
+    defile[i].addEventListener('click', () => {
+        toggledElement[i].classList.toggle('visible');
+    })
+}
 
