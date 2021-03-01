fdc66af9cd06d5a156f99dd0d70c9c8b76254dcb
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-03-01 20:42:27 +0100

    correction details

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,24 @@ const btn = document.querySelector('.enter');
 const bgImg = document.querySelector('.bg_img');
 btn.addEventListener('click', () => {
     bgImg.classList.add('fadeout')
-    // bgImg.style.display = 'none';
 })
 
 const defile = document.querySelectorAll('.defile');
+const icone = document.querySelectorAll('.fa-chevron-circle-down');
 const toggledElement = document.querySelectorAll('.toggle');
 for(let i = 0; i < defile.length; i++) {
     defile[i].addEventListener('click', () => {
         toggledElement[i].classList.toggle('visible');
+    })
+}
+for(let i = 0; i < icone.length; i++) {
+    icone[i].addEventListener('click', () => {
+        //icone[i].classList.toggle('rotate');
+        if (icone[i].style.transform === '') {
+            icone[i].style.transform = 'rotate(180deg)'
+        } else if (icone[i].style.transform === 'rotate(180deg)') {
+            icone[i].style.transform = ''
+        }
     })
 }
 
